9653bd95f0ea9094e22a6a39b3be600f2eaa9eb9
DarkLight19 <dan.imre.17f@szlgbp.hu>
2022-04-05 07:28:07 +0200

    2016

---
+++ E - 2016 Maj/otszaz/otszaz/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otszaz
+{
+    class Adat
+    {
+        public List<string> dolgok { get; set; }
+        public int db { get; set; }
+        public int fizetendo { get; set; }
+        public Adat()
+        {
+            dolgok = new List<string>();
+        }
+
+        public Adat(List<string> arucikkek)
+        {
+            dolgok = new List<string>(arucikkek);
+            db = arucikkek.Count;
+            fizetendoKiszamolas();
+        }
+
+        public void fizetendoKiszamolas()
+        {
+            fizetendo = 0;
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (var item in dolgok)
+            {
+                if(dict.ContainsKey(item))
+                {
+                    if (dict[item] == 1)
+                        fizetendo += 450;
+                    else
+                        fizetendo += 400;
+                }
+                else
+                {
+                    dict.Add(item, 1);
+                    fizetendo += 500;
+                }    
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. feladat:
+            Console.WriteLine("1. feladat:");
+
+            string[] s = System.IO.File.ReadAllLines("penztar.txt");
+            List<Adat> x = new List<Adat>();
+
+            List<string> temp = new List<string>();
+            foreach (var item in s)
+            {
+                if (item != "F")
+                {
+                    temp.Add(item);
+                    continue;
+                }
+
+                x.Add(new Adat(temp));
+                temp = new List<string>();
+            }
+            //2.feladat:
+            Console.WriteLine("2.feladat:");
+            Console.WriteLine(x.Count);
+            //3.feladat:
+            Console.WriteLine("3.feladat:");
+            Console.WriteLine(x[0].db);
+            //4.feladat:
+            Console.WriteLine("4.feladat:");
+
+            Console.WriteLine("Vásárlás sorszáma: ");
+            int sorszam = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Árucikk neve: ");
+            string arucikknev = Console.ReadLine();
+
+            Console.WriteLine("Darabszám: ");
+            int darabszam = int.Parse(Console.ReadLine());
+            //5.feladat:
+            Console.WriteLine("5.feladat:");
+            Console.Write("a) ");
+            bool elso = true;
+            int tempDb = 0;
+            int elsoSzam = 0;
+            int utolsoSzam = 0;
+            for (int i = 0; i < x.Count; i++)
+            {
+                if (x[i].dolgok.Contains(arucikknev))
+                {
+                    ++tempDb;
+
+                    if (elso)
+                    {
+                        elso = false;
+                        elsoSzam = i + 1;
+                    }
+
+                    utolsoSzam = i + 1;
+                }
+            }
+            Console.WriteLine(elsoSzam + " " + utolsoSzam);
+            Console.Write("b) ");
+            Console.WriteLine(tempDb);
+            //6.feladat:
+            Console.WriteLine("6.feladat:");
+            Console.WriteLine(ertek(darabszam));
+            //7.feladat:
+            Console.WriteLine("7.feladat:");
+            List<string> used = new List<string>();
+            foreach (var item in x[sorszam-1].dolgok)
+            {
+                if (used.Contains(item))
+                    continue;
+
+                Console.WriteLine(x[sorszam-1].dolgok.Where(k=>k.Contains(item)).Count() + " " + item);
+                used.Add(item);
+            }
+            //8.feladat:
+            Console.WriteLine("8.feladat:");
+            using (System.IO.StreamWriter f = new System.IO.StreamWriter("osszeg.txt"))
+            {
+                for (int i = 0; i < x.Count; i++)
+                    f.WriteLine(i + 1 + ": " + x[i].fizetendo);
+            }
+        }
+
+        public static int ertek(int db)
+        {
+            if (db == 1)
+                return 500;
+
+            return 950 + (db - 2) * 400;
+        }
+    }
+}
---
+++ E - 2016 Okt/telefon/telefon/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telefon
+{
+    class Program
+    {
+        class Adat
+        {
+            public int sorszam { get; set; }
+            public int varakozasiIdotartam { get; set; }
+            public int aktivIdotartam { get; set; }
+            public (int ora, int perc, int masodperc) mettol { get; set; }
+            public (int ora, int perc, int masodperc) felvevesIdo { get; set; }
+            public (int ora, int perc, int masodperc) meddig { get; set; }
+
+            public Adat()
+            {
+
+            }
+            public Adat(int sorszam, (int,int,int) mettol, (int, int, int) felvevesIdo, (int, int, int) meddig, int var, int akt)
+            {
+                this.sorszam = sorszam;
+                this.mettol = mettol;
+                this.felvevesIdo = felvevesIdo;
+                this.meddig = meddig;
+                varakozasiIdotartam = var;
+                aktivIdotartam = akt;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //1.feladat:
+            Console.WriteLine("1.feladat:");
+
+            //2.feladat:
+            Console.WriteLine("2.feladat:");
+            string[] s = System.IO.File.ReadAllLines("hivas.txt");
+            List<Adat> x = new List<Adat>();
+
+            (int ora, int perc, int masodperc) aktido = (8, 0, 0);
+            int counter = 0;
+            foreach (var item in s)
+            {
+                ++counter;
+                string[] temps = item.Split(' ');
+                (int ora, int perc, int masodperc) mettol = (int.Parse(temps[0]), int.Parse(temps[1]), int.Parse(temps[2]));
+                (int ora, int perc, int masodperc) meddig = (int.Parse(temps[3]), int.Parse(temps[4]), int.Parse(temps[5]));
+
+                if (mettol.ora >= 12 && mettol.perc >= 0 && mettol.masodperc > 0/*(aktido.ora >=12 && aktido.perc >= 0 && aktido.masodperc > 0) && mpbe(mettol.ora, mettol.perc, mettol.masodperc) > mpbe(aktido.ora, aktido.perc, aktido.masodperc)*/)
+                    break;
+
+                if (mpbe(meddig.ora, meddig.perc, meddig.masodperc) - mpbe(aktido.ora, aktido.perc, aktido.masodperc) >= 0)
+                {
+                    x.Add(new Adat(counter, mettol, aktido, meddig, (mpbe(aktido.ora, aktido.perc, aktido.masodperc) - mpbe(mettol.ora, mettol.perc, mettol.masodperc)), (mpbe(meddig.ora, meddig.perc, meddig.masodperc) - mpbe(aktido.ora, aktido.perc, aktido.masodperc))));
+                    aktido = meddig;
+                }
+                else
+                    x.Add(new Adat(counter, mettol, (-1, -1, -1), meddig, (mpbe(meddig.ora, meddig.perc, meddig.masodperc) - mpbe(mettol.ora, mettol.perc, mettol.masodperc)), -1));
+
+            }
+
+            Console.WriteLine(x.Count);
+
+            //3.feladat:
+            Console.WriteLine("3.feladat:");
+            {
+                Dictionary<int, int> dict = new Dictionary<int, int>();
+                foreach (var item in x)
+                {
+                    if (dict.ContainsKey(item.mettol.ora))
+                        ++dict[item.mettol.ora];
+                    else
+                        dict.Add(item.mettol.ora, 1);
+                    /*
+                    if (item.mettol.ora == item.meddig.ora)
+                        continue;
+
+                    if (dict.ContainsKey(item.meddig.ora))
+                        ++dict[item.meddig.ora];
+                    else
+                        dict.Add(item.meddig.ora, 1);*/
+                }
+
+                foreach (var item in dict)
+                    Console.WriteLine(item.Key + " " + item.Value);
+
+            }
+
+            //4.feladat:
+            Console.WriteLine("4.feladat:");
+            {
+                int max = x.Max(k => k.varakozasiIdotartam + k.aktivIdotartam);
+                foreach (var item in x)
+                {
+                    if (item.varakozasiIdotartam + item.aktivIdotartam != max)
+                        continue;
+
+                    Console.WriteLine(item.sorszam + " " + max);
+                    break;
+                }
+            }
+
+            //5.feladat:
+            Console.WriteLine("5.feladat:");
+            {
+                Console.Write("Adjon meg egy idopontot! (ora perc masodperc) ");
+                string[] temps = Console.ReadLine().Split(' ');
+                (int ora, int perc, int masodperc) bekert = (int.Parse(temps[0]), int.Parse(temps[1]), int.Parse(temps[2]));
+                int bekertMasodpercben = mpbe(bekert.ora, bekert.perc, bekert.masodperc);
+                Console.WriteLine("Hívó: " + x.Where(k => k.aktivIdotartam != -1 && mpbe(k.felvevesIdo.ora, k.felvevesIdo.perc, k.felvevesIdo.masodperc) < bekertMasodpercben && mpbe(k.meddig.ora, k.meddig.perc, k.meddig.masodperc) > bekertMasodpercben).ToList()[0].sorszam + ".");
+                Console.WriteLine("Várakozó: " + (x.Where(k => mpbe(k.mettol.ora, k.mettol.perc, k.mettol.masodperc) < bekertMasodpercben && mpbe(k.meddig.ora, k.meddig.perc, k.meddig.masodperc) > bekertMasodpercben).Count() - 1) + " db");
+            }
+
+            //6.feladat:
+            Console.WriteLine("6.feladat:");
+            {
+                List<Adat> temp = x.Where(k => k.felvevesIdo.ora != -1).ToList();
+                Console.WriteLine(temp[temp.Count - 1].sorszam + " " + temp[temp.Count - 1].varakozasiIdotartam);
+            }
+
+            //7.feladat:
+            Console.WriteLine("7.feladat:");
+            {
+                using (System.IO.StreamWriter f = new System.IO.StreamWriter(" sikeres.txt"))
+                {
+                    foreach (var item in x.Where(k=>k.aktivIdotartam != -1))
+                        f.WriteLine(item.sorszam + " " + item.felvevesIdo.ora + " " + item.felvevesIdo.perc + " " + item.felvevesIdo.masodperc + " " + item.meddig.ora + " " + item.meddig.perc + " " + item.meddig.masodperc);
+                }
+            }
+
+        }
+
+        public static int mpbe(int o, int p, int mp)
+        {
+            return o * 3600 + p * 60 + mp;
+        }
+    }
+}
